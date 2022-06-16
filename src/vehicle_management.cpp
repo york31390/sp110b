b1b9db2578937d08ca55a0dce3d947cdb46126af
york31390 <york31390@gmail.com>
2022-06-17 03:28:42 +0800

    新增與定義許多函式以及程式，組織大略結構

--- src/vehicle_management.cpp
+++ src/vehicle_management.cpp
@@ -36,3 +36,45 @@ typedef struct {
   vehicle_management_t target_data[DATABASE];
 } info_argc_t;
 
+unsigned int init_menu();
+
+// database
+unsigned int database_menu();
+void database(vehicle_management_t *data_p);
+void creat_database();
+void use_database(vehicle_management_t *data_p);
+void close_database(vehicle_management_t *data_p);
+
+// management
+unsigned int management_meun();
+void management(info_argc_t *info_p);
+void export_csv(vehicle_management_t *data_p);
+void insert(vehicle_management_t *data_p);
+void selete(vehicle_management_t *data_p);
+void drop(vehicle_management_t *data_p);
+void update(vehicle_management_t *data_p);
+
+// print example data
+void example(vehicle_management_t *data_p);
+
+// file merge to database
+unsigned int merge_menu();
+void merge(info_argc_t *info_p);
+void file_merge_data(info_argc_t *info_p);
+void data_merge_file(info_argc_t *info_p);
+void source_merge_to_target(vehicle_management_t *source_data_p,
+    vehicle_management_t *target_data_p, unsigned int *count_p);
+
+// Share function
+void keyin(vehicle_management_t *data_p, unsigned int *count_p);
+void keyin_row(vehicle_management_t *data_row_p);
+void search(vehicle_management_t *data_p);
+void print_data(vehicle_management_t *data_p, unsigned int *count_p);
+void print_row_data(vehicle_management_t data);
+void write_data(char *file_name_p, vehicle_management_t *data_p, unsigned int *count_p);
+void write_row_data(char *file_name_p, vehicle_management_t data);
+void read_data(info_argc_t *info_p);
+int compare(vehicle_management_t database_data_p, vehicle_management_t file_data_p);
+void print_part_of_data(info_argc_t *info_p);
+
+
